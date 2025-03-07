5071ed8a4b52cffde553dcef60c35b050ac1ce45
aegroto <aegroto@protonmail.com>
2025-03-07 23:23:30 +0100

    feat: complete window data fetching and basic formatting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::process::Command;
 
-use json::{object::Object, JsonValue};
+use serde_json::Value;
 
-fn json_cmd(command: &str, args: &[&str]) -> JsonValue {
+fn json_cmd(command: &str, args: &[&str]) -> Value {
     let output_str = String::from_utf8(
         Command::new(command)
             .args(args.to_vec())
@@ -16,24 +16,111 @@ fn json_cmd(command: &str, args: &[&str]) -> JsonValue {
         "Unable to convert command {command} {args:?} output to string"
     ));
 
-    json::parse(&output_str).expect("Unable to parse command {command} {args:?} output as JSON")
+    serde_json::from_str(&output_str)
+        .expect("Unable to parse command {command} {args:?} output as JSON")
 }
 
 fn main() {
-    let active_workspace_data: Object = match json_cmd("hyprctl", &["activeworkspace", "-j"]) {
-        JsonValue::Object(object) => object,
-        _ => panic!("hyprctl activeworkspace response is not a single JSON object"),
-    };
+    let active_windows = fetch_active_windows_data();
 
-    let active_workspace_id: i64 = match active_workspace_data
-        .get("id")
-        .expect("No field id in active workspace data")
-    {
-        JsonValue::Number(number) => number
-            .as_fixed_point_i64(0)
-            .expect("Unable to cast workspace id to i64"),
-        _ => panic!("Active workspace id is not a number"),
-    };
+    let output = active_windows
+        .iter()
+        .map(ActiveWindow::as_display_str)
+        .collect::<Vec<String>>()
+        .join(" | ");
 
-    println!("{active_workspace_id}")
+    println!("{output}")
+}
+
+struct ActiveWindow {
+    pub title: String,
+    pub app_name: String,
+    pub active: bool,
+    pub group_index: usize,
+}
+
+impl ActiveWindow {
+    fn from_json_data(data: Value, active_window_title: String) -> Self {
+        let group_index: usize = {
+            let id = data["address"]
+                .as_str()
+                .expect("Non-string window id")
+                .to_owned();
+
+            data["grouped"]
+                .as_array()
+                .expect("Non-array window group ids")
+                .to_owned()
+                .into_iter()
+                .map(|value| {
+                    value
+                        .as_str()
+                        .expect("Cannot cast window group id to string")
+                        .to_owned()
+                })
+                .position(|group_id| id == group_id)
+                .expect("Unable to find window id in group data")
+        };
+
+        let title = data["title"]
+            .as_str()
+            .expect("Non-string window title")
+            .to_owned();
+
+        Self {
+            active: title == active_window_title,
+            title,
+            group_index,
+            app_name: data["initialTitle"]
+                .as_str()
+                .expect("Non-string window initialTitle")
+                .to_owned(),
+        }
+    }
+
+    fn as_display_str(&self) -> String {
+        let appless_title = self.title.replace(&format!(" - {}", self.app_name), "");
+
+        if self.active {
+            format!("{}: {}", self.app_name, appless_title)
+        } else {
+            format!("{}", self.app_name)
+        }
+    }
+}
+
+fn fetch_active_windows_data() -> Vec<ActiveWindow> {
+    let active_workspace_data = json_cmd("hyprctl", &["activeworkspace", "-j"]);
+
+    let active_workspace_id = active_workspace_data["id"]
+        .as_i64()
+        .expect("Cannot get id from workspace data");
+
+    let active_window_title: String = active_workspace_data["lastwindowtitle"]
+        .as_str()
+        .expect("Cannot get active window title from workspace data")
+        .to_owned();
+
+    let windows_fetch_result = json_cmd("hyprctl", &["clients", "-j"]);
+
+    let active_windows_data: Vec<Value> = windows_fetch_result
+        .as_array()
+        .expect("Cannot convert hyprctl clients output as array")
+        .into_iter()
+        .cloned()
+        .filter(|window_data| {
+            window_data["workspace"]["id"]
+                .as_i64()
+                .expect("Missing id in window data")
+                .eq(&active_workspace_id)
+        })
+        .collect();
+
+    let mut windows = active_windows_data
+        .iter()
+        .cloned()
+        .map(|data| ActiveWindow::from_json_data(data, active_window_title.clone()))
+        .collect::<Vec<ActiveWindow>>();
+    windows.sort_by(|w1, w2| w1.group_index.cmp(&w2.group_index));
+    windows
 }
